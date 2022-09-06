c87f98adcd79361f6cb69f4f7ada3c7420b35df4
neko113 <alstn113@gmail.com>
2022-09-06 23:16:51 +0900

    ch04-traits

---
+++ ch04-traits/src/main.rs
@@ -0,0 +1,72 @@
+use std::fmt::Debug;
+
+struct Monster {
+    health: i32,
+}
+
+#[derive(Debug)]
+struct Wizard {
+    health: i32,
+}
+
+#[derive(Debug)]
+struct Elf {
+    health: i32,
+}
+
+trait Magic {}
+trait FightClose {}
+trait FightFromDistance {}
+
+impl Magic for Wizard {}
+impl FightClose for Wizard {}
+impl FightClose for Elf {}
+impl FightFromDistance for Elf {}
+
+fn attack_with_bow<T>(character: &T, oppenent: &mut Monster, distance: u32)
+where
+    T: FightFromDistance + Debug,
+{
+    if distance < 10 {
+        oppenent.health -= 10;
+        println!(
+            "Monster's health : {}, character : {:?}",
+            oppenent.health, character
+        );
+    }
+}
+
+fn attack_with_sword<T>(character: &T, oppenent: &mut Monster)
+where
+    T: FightClose + Debug,
+{
+    oppenent.health -= 20;
+    println!(
+        "Monster's health : {}, character : {:?}",
+        oppenent.health, character
+    );
+}
+
+fn attack_with_fireball<T>(character: &T, oppenent: &mut Monster, distance: u32)
+where
+    T: Magic + Debug,
+{
+    if distance < 15 {
+        oppenent.health -= 30;
+        println!(
+            "Monster's health : {}, character : {:?}",
+            oppenent.health, character
+        );
+    }
+}
+
+fn main() {
+    let wizard = Wizard { health: 80 };
+    let elf = Elf { health: 100 };
+
+    let mut monster = Monster { health: 100 };
+
+    attack_with_bow(&elf, &mut monster, 5);
+    attack_with_fireball(&wizard, &mut monster, 10);
+    attack_with_sword(&wizard, &mut monster);
+}
