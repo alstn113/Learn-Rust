9251a46f0b4572ac9438e50b51083a185b2e1e87
neko113 <alstn113@gmail.com>
2022-09-18 16:27:23 +0900

    project-greprs 반복자로 refactor

--- project/greprs/src/main.rs
+++ project/greprs/src/main.rs
@@ -6,9 +6,7 @@ use std::process;
 use greprs::Config;
 
 fn main() {
-    let args = env::args().collect::<Vec<String>>();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
