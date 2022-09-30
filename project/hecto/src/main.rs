32f6da528db4d5165e3de53cfd52795fb9107b82
neko113 <alstn113@gmail.com>
2022-09-30 11:44:06 +0900

    project Text Editor ch1

--- project/hecto/src/main.rs
+++ project/hecto/src/main.rs
@@ -1,11 +1,32 @@
-use std::io::{self, Read};
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
-        if c == 'q' {
-            break;
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?} \r", b)
+                } else {
+                    println!("{:?} {}\r", b, c)
+                }
+                if b == to_ctrl_byte('c') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
         }
     }
+}
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
 }
