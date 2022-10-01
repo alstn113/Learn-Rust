1ec156ee8257107bedc7869f57f7b4dc21087ca7
neko113 <alstn113@gmail.com>
2022-10-01 13:48:00 +0900

    project - hecto ch3

---
+++ project/hecto/src/editor.rs
@@ -0,0 +1,126 @@
+use crate::Terminal;
+use termion::event::Key;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+pub struct Editor {
+    should_quit: bool,
+    terminal: Terminal,
+    cursor_position: Position,
+}
+
+impl Editor {
+    pub fn default() -> Self {
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+            cursor_position: Position { x: 0, y: 0 },
+        }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            if let Err(error) = self.refresh_screen() {
+                die(error);
+            }
+            if self.should_quit {
+                break;
+            }
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
+        }
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::cursor_hide();
+        Terminal::cursor_position(&Position { x: 0, y: 0 });
+        if self.should_quit {
+            Terminal::clear_screen();
+            print!("Goodbye.\r");
+        } else {
+            self.draw_rows();
+            Terminal::cursor_position(&self.cursor_position);
+        }
+        Terminal::cursor_show();
+        Terminal::flush()
+    }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = Terminal::read_key()?;
+        match pressed_key {
+            Key::Ctrl('c') => self.should_quit = true,
+            Key::Up
+            | Key::Down
+            | Key::Left
+            | Key::Right
+            | Key::PageUp
+            | Key::PageDown
+            | Key::Home
+            | Key::End => self.move_cursor(pressed_key),
+            _ => (),
+        }
+        Ok(())
+    }
+
+    fn move_cursor(&mut self, key: Key) {
+        let Position { mut x, mut y } = &mut self.cursor_position;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
+        match key {
+            Key::Up => y = y.saturating_sub(1),
+            Key::Down => {
+                if y < height {
+                    y = y.saturating_add(1);
+                }
+            }
+            Key::Left => x = x.saturating_sub(1),
+            Key::Right => {
+                if x < width {
+                    x = x.saturating_add(1);
+                }
+            }
+            Key::Home => x = 0,
+            Key::End => x = width,
+            Key::PageUp => y = 0,
+            Key::PageDown => y = height,
+            _ => (),
+        }
+        self.cursor_position = Position { x, y };
+    }
+
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_add(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
+    }
+
+    fn draw_rows(&self) {
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
+        }
+    }
+}
+
+// 에러 처리를 위함.
+fn die(e: std::io::Error) {
+    Terminal::clear_screen();
+    panic!("{}", e);
+}
--- project/hecto/src/main.rs
+++ project/hecto/src/main.rs
@@ -1,35 +1,10 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
+mod editor;
+mod terminal;
+
+use editor::Editor;
+pub use editor::Position;
+pub use terminal::Terminal;
 
 fn main() {
-    // 바로 바로 칠 수 입력받을 수 있음.
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("{:?} \r", b)
-                } else {
-                    println!("{:?} {}\r", b, c)
-                }
-                if b == to_ctrl_byte('c') {
-                    break;
-                }
-            }
-            Err(err) => die(err),
-        }
-    }
-}
-
-// ctrl + key 처리를 위함.
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
-
-// 에러 처리를 위함.
-fn die(e: std::io::Error) {
-    panic!("{}", e);
+    Editor::default().run();
 }
