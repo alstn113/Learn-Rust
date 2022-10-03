633c7d7044c608bf408af1f7a7d906188f2f9280
neko113 <alstn113@gmail.com>
2022-10-03 19:54:24 +0900

    Invert tab to space

--- project/hecto/src/row.rs
+++ project/hecto/src/row.rs
@@ -1,14 +1,19 @@
 use std::cmp;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub struct Row {
     string: String,
+    len: usize,
 }
 
 impl From<&str> for Row {
     fn from(slice: &str) -> Self {
-        Self {
+        let mut row = Self {
             string: String::from(slice),
-        }
+            len: 0,
+        };
+        row.update_len();
+        row
     }
 }
 
@@ -16,14 +21,31 @@ impl Row {
     pub fn render(&self, start: usize, end: usize) -> String {
         let end = cmp::min(end, self.string.len());
         let start = cmp::min(start, end);
-        self.string.get(start..end).unwrap_or_default().to_string()
+        let mut result = String::new();
+        for grapheme in self.string[..]
+            .graphemes(true)
+            .skip(start)
+            .take(end - start)
+        {
+            // Rust에서는 Tab(8칸)이 중요하지 않기때문에 그냥 공백으로 변경
+            if grapheme == "\t" {
+                result.push_str(" ");
+            } else {
+                result.push_str(grapheme);
+            }
+        }
+        result
     }
 
     pub fn len(&self) -> usize {
-        self.string.len()
+        self.len
     }
 
     pub fn is_empty(&self) -> bool {
-        self.string.is_empty()
+        self.len == 0
+    }
+
+    fn update_len(&mut self) {
+        self.len = self.string[..].graphemes(true).count();
     }
 }
