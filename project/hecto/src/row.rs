4758cc2696bddbd2c86c9dd421d76f7a2b20793f
neko113 <alstn113@gmail.com>
2022-10-03 23:27:51 +0900

    complex deletion

--- project/hecto/src/row.rs
+++ project/hecto/src/row.rs
@@ -69,4 +69,9 @@ impl Row {
         }
         self.update_len();
     }
+
+    pub fn append(&mut self, new: &Self) {
+        self.string = format!("{}{}", self.string, new.string);
+        self.update_len();
+    }
 }
