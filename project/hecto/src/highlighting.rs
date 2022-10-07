fc3269ba1edcfa58ec4e93400e4479f8d90ef094
neko113 <alstn113@gmail.com>
2022-10-07 21:49:22 +0900

    Refactor syntax highlighting

--- project/hecto/src/highlighting.rs
+++ project/hecto/src/highlighting.rs
@@ -4,12 +4,14 @@ use termion::color;
 pub enum Type {
     None,
     Number,
+    Match,
 }
 
 impl Type {
     pub fn to_color(&self) -> impl color::Color {
         match self {
             Self::Number => color::Rgb(220, 163, 163),
+            Self::Match => color::Rgb(38, 139, 210),
             _ => color::Rgb(225, 225, 226),
         }
     }
